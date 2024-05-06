c3f4313ccb5113600e491353b589997721481190
J. M. Ifthakharul Islam Shajan <137435768+if-i-shajan@users.noreply.github.com>
2024-05-06 22:24:57 +0600

    Here the code

--- Library_Management.c
+++ Library_Management.c
@@ -1,1 +1,32 @@
 #include<stdio.h>
+int main()
+{
+    printf("\n\n");
+    printf("\t\t\t***********************************************\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*        ----------------------------         *\n");
+    printf("\t\t\t*             WELCOME TO LIBRARY              *\n");
+    printf("\t\t\t*        ----------------------------         *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t***********************************************\n");
+    printf("\n\n");
+    printf("\t\t\t***********************************************\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*   ---------------------------------------   *\n");
+    printf("\t\t\t*      DAFFODIL INTERNATIONAL UNIVERSITY      *\n");
+    printf("\t\t\t*   ---------------------------------------   *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t*             Birulia,Savar,Dhaka             *\n");
+    printf("\t\t\t*   Email: librarian@daffodilvarsity.edu.bd   *\n");
+    printf("\t\t\t*                                             *\n");
+    printf("\t\t\t***********************************************\n");
+    printf("\n\n\t\t\t          Press any key to continue: ");
+    
+}
+                                                          
+  
+  
+  
